125843cdf059fdb44d9a879d04826816dbdfb4c8
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-03-09 19:10:58 +0000

    Configuration test

--- .release-it.js
+++ .release-it.js
@@ -4,7 +4,59 @@ module.exports = {
       gitRawCommitsOpts: {
         path: ".",
       },
-      header: "# Changelog\n\nThis changelog is updated automatically using release-it.\n",
+      whatBump: (commits, options) => {
+        let defaults = {
+          build: "ignore",
+          feat: "ignore",
+          ci: "ignore",
+          docs: "ignore",
+          feat: "ignore",
+          fix: "ignore",
+          perf: "ignore",
+          refactor: "ignore",
+          test: "ignore",
+          chore: "patch",
+          choredev: "ignore",
+        };
+
+        let types = (options?.preset?.types || []).reduce((a, v) => {
+          return { ...a, [v.type]: v.release };
+        }, {});
+
+        types = Object.assign({}, defaults, types);
+
+        let breakings = 0;
+        let features = 0;
+
+        let levelSet = ["major", "minor", "patch", "ignore"];
+
+        let level = Math.min.apply(
+          Math,
+          commits.map((commit) => {
+            let level = levelSet.indexOf(types[commit.type]);
+
+            level = level < 0 ? 3 : level;
+            if (commit.notes.length > 0) {
+              breakings += commit.notes.length;
+            }
+
+            if (commit.type === "feat") {
+              features += 1;
+            }
+            return level;
+          })
+        );
+
+        return {
+          level: level,
+          reason:
+            breakings === 1
+              ? `There is ${breakings} BREAKING CHANGE and ${features} features`
+              : `There are ${breakings} BREAKING CHANGES and ${features} features`,
+        };
+      },
+      header:
+        "# Changelog\n\nThis changelog is updated automatically using release-it.\n",
       infile: "CHANGELOG.md",
       path: ".",
       preset: {
@@ -13,46 +65,62 @@ module.exports = {
           {
             section: "Features",
             type: "feat",
+            release: "minor",
           },
           {
             section: "Bug Fixes",
             type: "fix",
+            release: "patch",
           },
           {
             section: "Tests",
             type: "test",
+            release: "ignore",
           },
           {
             section: "Chores",
             type: "chore",
+            release: "patch",
           },
           {
             section: "Documentation",
             type: "docs",
+            release: "ignore",
           },
           {
             section: "Performance Improvements",
             type: "perf",
+            release: "patch",
           },
           {
             section: "Code Refactoring",
             type: "refactor",
+            release: "patch",
           },
           {
             section: "Code Style Changes",
             type: "style",
+            release: "patch",
           },
           {
             section: "Build Changes",
             type: "build",
+            release: "patch",
           },
           {
             section: "Continuous Integration",
             type: "ci",
+            release: "patch",
           },
           {
             section: "Reverts",
             type: "revert",
+            release: "patch",
+          },
+          {
+            section: "Chore DevDependencies",
+            type: "choredev",
+            release: "ignore",
           },
         ],
       },
